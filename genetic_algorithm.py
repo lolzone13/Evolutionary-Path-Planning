971a53432dcdbdf7baa218b8c63178d8ffb6c891
lolzone13 <mohitscience123@gmail.com>
2022-10-30 00:42:48 +0530

    Added mutation, crossover

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,4 +1,5 @@
 import random
+import math
 from path_map import Path_Map
 
 course_map = Path_Map()
@@ -18,7 +19,6 @@ Comments
 class Individual:
     def __init__(self, num_possible_moves, moves=[], robot_height=3):
         self.moves = moves
-        self.possible_moves = ['U', 'L', 'R', 'D']
         self.num_moves = len(moves)
         self.robot_height = robot_height
 
@@ -32,7 +32,6 @@ class Individual:
 
         self.fitness = self.fitness_function()
 
-
     def fitness_function(self):
         f = 0
         x, y = course_map.start
@@ -44,7 +43,7 @@ class Individual:
                 if y > 0 and height + self.robot_height >= course_map.map[x][y-1]:
                     f = f + (course_map.map[x][y-1] - height + 1)
                     y = y - 1
-                
+
             elif (self.moves[m] == 'U'):
                 if x > 0 and height + self.robot_height >= course_map.map[x-1][y]:
                     f = f + (course_map.map[x-1][y] - height + 1)
@@ -60,7 +59,6 @@ class Individual:
                     f = f + (course_map.map[x+1][y] - height + 1)
                     x = x + 1
 
-
             if x == tx and y == ty:  # award the individual that gets to the final point
                 f = f + len(self.moves)
 
@@ -70,10 +68,53 @@ class Individual:
         return f
 
 
+class Population:
+    def __init__(self, population_size, num_possible_moves):
+        self.population = []
+
+        for _ in range(population_size):
+            self.population.append(Individual(num_possible_moves))
+
 
 class Genetic_Algorithm:
     def __init__(self, population_size, iterations=10):
         self.population_size = population_size
         self.iterations = iterations
 
-    # def crossover(self):
+    def roulette_selection(self, fitness_values):
+        total_fitness = sum(fitness_values)
+        probabilities = [math.ceil((v/total_fitness) * 100)
+                         for v in fitness_values]
+
+        selection_weights = []
+        accumulated_sum = 0
+
+        for p in probabilities:
+            accumulated_sum += p
+            selection_weights.append(accumulated_sum)
+
+        # spin roulette wheel and generate solution
+        p = random.randint(0, selection_weights[-1])
+        for i in range(len(selection_weights)):
+            if (selection_weights[i] >= p):
+                return i
+
+    def crossover(self, parent1, parent2):
+        num = random.randint(0, parent1.num_moves-1)
+        child1 = Individual(parent1.num_moves,
+                            parent1.moves[0:num] + parent2.moves[num:])
+        child2 = Individual(parent2.num_moves,
+                            parent2.moves[0:num] + parent1.moves[num:])
+
+        return (child1, child2)
+
+    # def adaptive_mutation():
+
+    def mutation(self, mutation_probability, individual):
+        p = random.random()
+
+        if (p < mutation_probability):
+            index = random.randint(0,individual.num_moves-1)
+            individual.moves[index] = random.choice('LURD')
+        
+        return individual
